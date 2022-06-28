7bcb4ed15bbff892a2bb42630388d083d6705229
Arseniy <ar7eniy.k@gmail.com>
2022-06-28 08:59:20 +0300

    Add custom packet format parsing

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import struct
 import sys
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional
+import enum
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 from serial import Serial, SerialException
 from serial.tools.list_ports import comports
 
 from ui.design import Ui_MainWindow
+from util import crc16_xmodem
 
 
 class SerialManager:
+    """
+    Provides non-blocking access to serial port for parsing telemetry packets.
+    """
+
+    class State(enum.Enum):
+        """
+        State of COBS decoder.
+        """
+        SCAN = 0  # next byte will be read literal
+        ESC = 1   # next byte is COBS-escaped character
+        START = 2  # next byte is start of the packet
 
     @dataclass
     class Packet:
@@ -26,19 +39,23 @@ class SerialManager:
         i_term: float
         d_term: float
 
+    state: State
     serial: Serial
     port: Optional[str]
-    baudrate: int
-    PACKET_LENGTH = 29
-    PACKET_TERMINATOR = b"\x0A"
+    baudrate: Optional[int]
+    cobs_code: int
+    packet: bytearray
+
+    PACKET_LENGTH = 28
+    PACKET_TERMINATOR = 0x00
 
     def __init__(self):
-        self.serial = Serial()
+        self.serial = Serial(timeout=0)  # non-blocking access
         self.port = None
-        self.baudrate = 0
+        self.baudrate = None
 
     @staticmethod
-    def get_ports() -> List[str]:
+    def get_ports() -> list[str]:
         return [port.device for port in comports()]
 
     def connect(self):
@@ -50,6 +67,8 @@ class SerialManager:
         self.serial.open()
         if not self.serial.isOpen():
             raise SerialException(f"Could not open serial port {self.port}")
+        self.packet = bytearray()
+        self.state = self.State.START
 
     def disconnect(self):
         self.serial.close()
@@ -57,25 +76,75 @@ class SerialManager:
     def connected(self) -> bool:
         return self.serial.isOpen()
 
-    def get_packet(self) -> Optional[Packet]:
+    def update(self, c) -> Optional[Packet]:
         """
-        Reads a packet from the serial port and parses it into a Packet object.
+        Update packet parser with character `c` and try to parse a Packet object from internal buffer.
+        If no complete packet is available, returns None. Raises IOError if the received packet is not valid.
+        Uses COBS for packet framing. (see https://en.wikipedia.org/wiki/Consistent_Overhead_Byte_Stuffing)
         Packet format:
-        +--------------------+--------------------+--------------------+--------------------+
-        |  Input (float32)   | Setpoint (float32) |   Error (float32)  |   Gain (float32)   |
-        +--------------------+--------------------+--------------------+------+-------------+
-        |  P Term (float32)  | I Term (float32)   | D Term (float32)   | 0x0A |
-        +--------------------+--------------------+--------------------+------+
-        0x0A is a packet terminator.
-        Packet length: (4 * 7) + 1 = 29 bytes
+        +--------------------+---------//-----+--------------------------------+------+
+        | COBS overhead byte | payload // ... | CRC-CCITT of payload (2 bytes) | 0x00 |
+        +--------------------+---------//-----+------------- ------------------+------+
+        NOTE: For packet creation, at first, checksum of payload is computed,
+              after that payload + checksum gets encoded with COBS
+        0x00 is a packet terminator.
         """
-        data: bytes = self.serial.read_until(self.PACKET_TERMINATOR)
-        # if line is desynced, discard the packet
-        # now line is in sync, the next byte is the beginning of the next packet
-        if len(data) != SerialManager.PACKET_LENGTH:
+        packet = None
+
+        match (self.state, c):
+            case (self.State.START, _):
+                # start of packet byte does not represent any payload character
+                self.cobs_code = c
+                self.state = self.State.SCAN
+
+            case (self.State.SCAN, self.PACKET_TERMINATOR):
+                # unexpected packet termination, drop this packet
+                print(f"[!] Unexpected packet termination, received part: {self.packet.hex(' ')}")
+                self.state = self.State.START  # start over
+                self.packet = bytearray()
+
+            case (self.State.ESC, self.PACKET_TERMINATOR):
+                # packet is complete, process it
+                packet = self.parse_packet(self.packet)
+                self.state = self.State.START
+                self.packet = bytearray()
+
+            case (self.State.SCAN, _):
+                self.packet.append(c)
+
+            case (self.State.ESC, _):
+                self.packet.append(self.PACKET_TERMINATOR)
+                self.cobs_code = c
+                self.state = self.State.SCAN
+
+        if self.cobs_code == 1:
+            # next byte is COBS-escaped
+            self.state = self.State.ESC
+
+        self.cobs_code -= 1
+        return packet
+
+    @staticmethod
+    def parse_packet(packet: bytearray) -> Optional[Packet]:
+        """
+        Verify checksum of packet and parse it into a Packet object.
+        If checksum is invalid, returns None.
+        Checksum is the last two bytes of the packet, CRC-XMODEM is used.
+        """
+        if len(packet) != SerialManager.PACKET_LENGTH + 2:
             return None
-        data.rstrip(self.PACKET_TERMINATOR)
-        return SerialManager.Packet(*struct.unpack("<7f", data))
+
+        packet_payload = packet[:-2]
+        packet_checksum = struct.unpack("<H", packet[-2:])[0]
+        real_checksum = crc16_xmodem(packet_payload)
+
+        print(f"Received packet: {packet_payload.hex(' ')}, checksum: {hex(packet_checksum)}")
+        print(f"Calculated checksum: {hex(real_checksum)}")
+
+        if packet_checksum != real_checksum:
+            return None
+
+        return SerialManager.Packet(*struct.unpack("<7f", packet_payload))
 
 
 class MainWindow(QMainWindow):
@@ -101,7 +170,7 @@ class MainWindow(QMainWindow):
         self.ui.portErrorLabel.hide()
         self.ui.portError.hide()
 
-        ports: List[str] = self.serial.get_ports()
+        ports: list[str] = self.serial.get_ports()
 
         self.ui.selectPortBox.clear()
         if ports:
